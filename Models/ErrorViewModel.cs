9f010c5d9a83ca4c1709a3e0ec8855df01ea7bad
XiaoMaoBao <xzhang1994@gmail.com>
2019-11-18 14:00:54 +0100

    Frontend initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace thehaguetech_community_portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
