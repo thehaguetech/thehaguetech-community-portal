0c4ca460ef414442767064fbb79d7245bc4173d0
SimonSprangers <47181836+SimonSprangers@users.noreply.github.com>
2019-11-25 13:27:17 +0100

    TopNav
    Sidebar added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -42,6 +42,10 @@ namespace thehaguetech_community_portal.Controllers
         {
             return View();
         }
+        public IActionResult Event()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
